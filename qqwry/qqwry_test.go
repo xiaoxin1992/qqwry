7ae6142e607494c15f5ba73ddb3ecaf7581f8983
hexinhai <hexinhai@100tal.com>
2020-09-09 13:15:39 +0800

    更新版本信息

--- qqwry/qqwry_test.go
+++ qqwry/qqwry_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestNewQQWry(t *testing.T) {
-	q, err := NewQQWry("./qqwry.dat")
+	q, err := NewQQWry("../qqwry.dat")
 	if err != nil {
 		log.Println(err)
 		return
@@ -15,7 +15,7 @@ func TestNewQQWry(t *testing.T) {
 	result := q.Match("192.168.1.1")
 	log.Println(result)
 	// 数据转换成map格式，转换成JSON格式，写入到文件
-	err = q.ConvertMap().DumpToJson("test.json")
+	err = q.ConvertMap().DumpToJson("../test.json")
 	if err != nil {
 		log.Println(result)
 	}
